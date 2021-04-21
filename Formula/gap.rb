652b41cbf849415547d12bf3b86f1a77f7c83961
Alexander Konovalov <alexk@mcs.st-andrews.ac.uk>
2021-04-21 16:43:26 +0100

    Upgrade to GAP 4.11.1

--- Formula/gap.rb
+++ Formula/gap.rb
@@ -1,8 +1,8 @@
 class Gap < Formula
   desc "System for computational discrete algebra"
   homepage "https://www.gap-system.org/"
-  url "https://www.gap-system.org/pub/gap/gap-4.11/tar.bz2/gap-4.11.0.tar.bz2"
-  sha256 "bf070a10dc2ac60593fe65df0f873d72d4d67501e86eb262a6f6bd48fc868602"
+  url "https://github.com/gap-system/gap/releases/download/v4.11.1/gap-4.11.1.tar.gz"
+  sha256 "6635c5da7d82755f8339486b9cac33766f58712f297e8234fba40818902ea304"
 
   depends_on "gmp"
   # GAP cannot be built against the native macOS version of readline
